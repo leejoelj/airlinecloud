adbf421b81623b50087a2e332a3da122786852fd
leejoelj <leejoe1101@gmail.com>
2017-09-28 14:41:05 +0800

    change route

--- application/config/routes.php
+++ application/config/routes.php
@@ -1,6 +1,5 @@
 <?php
 defined('BASEPATH') OR exit('No direct script access allowed');
-
 /*
 | -------------------------------------------------------------------------
 | URI ROUTING
@@ -68,4 +67,4 @@ $route['default_controller'] = 'pages/view';
 
 $route['(:any)'] = 'pages/view/$1';
 $route['404_override'] = '';
-$route['translate_uri_dashes'] = FALSE;
+$route['translate_uri_dashes'] = TRUE;
